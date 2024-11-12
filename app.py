a72c193052e419f97e47335e365e6261ca2d86f0
Nemuel Wainaina <nemuelwainaina@proton.me>
2024-11-12 07:53:51 +0300

    Fix the missing module runtime error

--- app.py
+++ app.py
@@ -213,7 +213,7 @@ class AboutDialog(QDialog):
         title_label.setAlignment(Qt.AlignCenter)
         layout.addWidget(title_label)
 
-        info_label = QLabel('Version 1.2.1\nUnofficial desktop app for Proton.')
+        info_label = QLabel('Version 1.2.2\nUnofficial desktop app for Proton.')
         info_label.setAlignment(Qt.AlignCenter)
         layout.addWidget(info_label)
 
